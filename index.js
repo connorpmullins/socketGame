fa99c9f45049d022a957cbbea16d0d6895787694
Connor Mullins <connorpmullins@gmail.com>
2021-12-22 04:29:30 -0700

    tear out webpack

--- index.js
+++ index.js
@@ -1,21 +1,18 @@
 const express = require('express')
 const path = require('path')
 const { Server } = require('socket.io')
-const game = require('../src/game')
+const game = require('./public/game')
 
 const app = express()
 const PORT = 3000
 
-app.use(express.static(path.join(__dirname, '..', 'dist')))
+app.use(express.static(path.join(__dirname, 'public')))
 // app.use(express.static('public'))
 
-app.use((req, res, next) => {
-  res.sendFile(path.join(__dirname, '..', 'public', 'server.html'))
+const server = app.listen(PORT, () => {
+  console.log('app.listen')
 })
 
-const server = app.listen(PORT, () => {
-  console.log('server')
-})
 const io = new Server(server)
 
 io.sockets.on('connection', (socket) => {
